7ef832be4bda4a66b5717f17f9c24a1d796898d3
mao wang <lwang2819@gmail.com>
2026-07-22 20:04:29 +0200

    aggiunto nel commit il file .h

--- src/header/linearizer.h
+++ src/header/linearizer.h
@@ -1,9 +1,11 @@
 #ifndef LINEARIZER_H
 #define LINEARIZER_H
+#include <stdbool.h>
 
 #include <stdio.h>
 
 char** linearize(FILE*);
 void free_linearization(char**);
+bool TypePresent(char*, char**);
 
 #endif
